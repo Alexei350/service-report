24b383937389a0993b58094658e2a0cc5d4f4bd1
Alexei <alexeisecretti@hotmail.com>
2019-04-12 13:30:09 -0300

    Update index.php

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
 		</header>
 
 	  	<main role="main" class="inner cover">
-		    <h1 class="cover-heading">Cover your page.</h1>
+		    <h1 class="cover-heading">OS Report</h1>
 		    <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
 		    <p class="lead">
 		      	<a href="#" class="btn btn-lg btn-secondary">Learn more</a>
