a99e680f99fe18f6d04444fde06bd4ac4f1cdbe4
Alexei350 <alexeisecretti@hotmail.com>
2019-05-11 22:08:23 -0300

    Busca no banco

--- pesquisa.php
+++ pesquisa.php
@@ -2,7 +2,8 @@
 <html lang="br">
 	<head>
 		<title>OS Report Beta v0.1</title>
-	  	<meta charset="UTF-8">
+		<meta http-equiv="content-Type" content="text/html; charset=iso-8859-1" /> 
+	  	<!--meta charset="UTF-8"-->
 
 	  	<link rel="shortcut icon" href="/img/logo-dark.png">
 
@@ -25,26 +26,56 @@
 		    </div>
 		</header>
 
+		<!--?php
+			$mysqli = new mysqli("127.0.0.1", "root", "", "projeto", 3306);
+
+			$res = $mysqli->query("SELECT * FROM report_os ORDER BY id ASC");
+			$res->data_seek(0);
+
+			$vet = array();
+			$i = 0;
+			while ($row = $res->fetch_assoc()) 
+			{
+				$vet[i][0] = $row['Valor'];
+				$vet[i][1] = $row['Tempo'];
+				$vet[i][2] = $row['Desc'];
+				$i++;
+			}
+		?-->
+
 	  	<main class="inner cover">
 	  		<div class="row justify-content-md-center" id="principal">
 		  		<div class="col-lg-6">
-		  			<div class="row">
-						<div class="jumbotron jumbotron-fluid">
-							<div class="container">
-							  	<h3>João da Silva - Conserto de ar condicionado</h1>
-							  	<p class="lead">Valor: R$ 120,00<br>Tempo: 4h<br>Descrição detalhada: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nunc purus, ornare ornare bibendum ac, fermentum at massa. Nam rutrum eros sit amet turpis tempus faucibus. Phasellus eget dui vitae sem molestie euismod lobortis ut magna. Nullam commodo tortor tempus felis euismod molestie.</p>
-							</div>
-						</div>
-				    </div>
+					<?
+						$mysqli = new mysqli("127.0.0.1", "root", "", "projeto", 3306);
 
-				    <div class="row">
-						<div class="jumbotron jumbotron-fluid">
-							<div class="container">
-							  	<h3>José Alberto - Instalação de chuveiro</h1>
-							  	<p class="lead">Valor: R$ 50,00<br>Tempo: 1h<br>Descrição detalhada: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nunc purus, ornare ornare bibendum ac, fermentum at massa. Nam rutrum eros sit amet turpis tempus faucibus. Phasellus eget dui vitae sem molestie euismod lobortis ut magna. Nullam commodo tortor tempus felis euismod molestie.</p>
-							</div>
-						</div>
-				    </div>
+						$res = $mysqli->query("SELECT *
+						
+												FROM report_os as os 
+												
+											INNER JOIN report_cliente as cliente 
+														ON os.ID_Cliente = cliente.ID 
+														
+											ORDER BY os.ID ASC");
+						$res->data_seek(0);
+
+						while ($row = $res->fetch_assoc()) 
+						{
+							echo "	<div class='row'>
+										<div class='jumbotron jumbotron-fluid'>
+											<div class='container'>
+												<h3>{$row['Nome']} - {$row['Servico']}</h3>
+												
+												<p class = 'lead'>
+													Valor: R$ {$row['Valor']}<br>
+													Tempo: {$row['Tempo']}h<br>
+													Descrição detalhada: {$row['Descricao']}
+												</p>
+											</div>
+										</div>
+									</div>";
+						}
+					?>
 				</div>
 			</div>
 		</main>
