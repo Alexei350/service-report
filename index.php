576412adbc22038a8a9c66b17f15fc117b6175b6
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-05-16 16:37:05 -0300

    Move código CSS do arquivo index.php para o cover.css

--- index.php
+++ index.php
@@ -11,24 +11,6 @@
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 
-		<style>
-	    	.bd-placeholder-img {
-	    	  	font-size: 1.125rem;
-	    	  	text-anchor: middle;
-	    	  	-webkit-user-select: none;
-	    	  	-moz-user-select: none;
-	    	  	-ms-user-select: none;
-	    	  	user-select: none;
-	    	}
-
-	    	@media (min-width: 768px) 
-	    	{
-	    		.bd-placeholder-img-lg 
-	    	  	{
-	    	  		font-size: 3.5rem;
-	    	  	}
-	    	}
-	    </style>
 	    <link href="cover.css" rel="stylesheet">
 	</head>
 
