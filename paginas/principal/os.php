b71ee875171d7567ba88246f25115f4a4c80becb
Alexei <alexeisecretti@hotmail.com>
2019-06-05 10:46:54 -0300

    Altera ordem dos campos e simplifica código
    Altera ordem dos campos na tela de cadastro de os e chama o javascript de validação na tela de registro de usuário.

--- paginas/principal/os.php
+++ paginas/principal/os.php
@@ -1,6 +1,5 @@
 <style>
-	.bd-placeholder-img 
-	{
+	.bd-placeholder-img {
 		font-size: 1.125rem;
 		text-anchor: middle;
 		-webkit-user-select: none;
@@ -9,29 +8,22 @@
 		user-select: none;
 	}
 
-	@media (min-width: 768px) 
-	{
-		.bd-placeholder-img-lg 
-		{
+	@media (min-width: 768px) {
+		.bd-placeholder-img-lg {
 			font-size: 3.5rem;
 		}
 	}
 
-	#os
-	{
+	#os {
 		max-width: 960px;
 	}
 
-	.lh-condensed 
-	{ 
-		line-height: 1.25; 
+	.lh-condensed {
+		line-height: 1.25;
 	}
 </style>
 
 <script src="/scripts/validacao.js"></script>
-
-
-
 
 <div class="row justify-content-md-center">
 	<div class="col-lg-6">
@@ -39,29 +31,29 @@
 			<img class="d-block mx-auto mb-4" src="/img/os.png" alt="" width="72" height="72">
 			<h2>Cadastro de Ordens de Serviço</h2>
 			<p class="lead">Forneça os dados do cliente, descrição dos serviços e informações para adicionar a ordem de serviço.</p>
-		</div>		  	
+		</div>
 	</div>
 </div>
 
 <div class="row justify-content-md-center">
 	<div class="col-md-6 align-self-start">
 		<h4 class="mb-3">Dados do cliente</h4>
-		
+
 		<form class="needs-validation" novalidate>
 			<div class="row">
 				<div class="col-md-6 mb-3">
 					<label for="firstName">Primeiro nome</label>
 					<input type="text" class="form-control" id="firstName" placeholder="" value="" required>
-					
+
 					<div class="invalid-feedback">
 						Primeiro nome válido é necessário.
 					</div>
 				</div>
-				
+
 				<div class="col-md-6 mb-3">
 					<label for="lastName">Sobrenome</label>
 					<input type="text" class="form-control" id="lastName" placeholder="" value="" required>
-					
+
 					<div class="invalid-feedback">
 						Sobrenome válido é necessário.
 					</div>
@@ -71,22 +63,22 @@
 			<div class="mb-3">
 				<label for="email">Email <span class="text-muted">(Opcional)</span></label>
 				<input type="email" class="form-control" id="email" placeholder="Ex: usuario@dominio.com">
-				
+
 				<div class="invalid-feedback">
 					Por favor insira um email válido.
 				</div>
 			</div>
 
-			<div class="mb-3">
-				<label for="address">Endereço</label>
-				<input type="text" class="form-control" id="address" placeholder="Ex: Av. Brasil, 123" required>
-				
-				<div class="invalid-feedback">
-					Por favor insira o endereço.
-				</div>
-			</div>
-
 			<div class="row">
+				<div class="col-md-3 mb-3">
+					<label for="zip" ´>CEP</label>
+					<input type="text" class="form-control" id="zip" placeholder="" required>
+
+					<div class="invalid-feedback">
+						Insira um CEP válido.
+					</div>
+				</div>
+
 				<div class="col-md-5 mb-3">
 					<label for="country">País</label>
 					<select class="custom-select d-block w-100" id="country" required>
@@ -97,7 +89,7 @@
 						Por favor selecione um país válido.
 					</div>
 				</div>
-				
+
 				<div class="col-md-4 mb-3">
 					<label for="state">Estado</label>
 					<select class="custom-select d-block w-100" id="state" required>
@@ -130,19 +122,19 @@
 						<option>Sergipe (SE)</option>
 						<option>Tocantins (TO)</option>
 					</select>
-					
+
 					<div class="invalid-feedback">
 						Por favor selecione um estado válido.
 					</div>
 				</div>
-				
-				<div class="col-md-3 mb-3">
-					<label for="zip"´>CEP</label>
-					<input type="text" class="form-control" id="zip" placeholder="" required>
-					
-					<div class="invalid-feedback">
-						Insira um CEP válido.
-					</div>
+			</div>
+
+			<div class="mb-3">
+				<label for="address">Endereço</label>
+				<input type="text" class="form-control" id="address" placeholder="Ex: Av. Brasil, 123" required>
+
+				<div class="invalid-feedback">
+					Por favor insira o endereço.
 				</div>
 			</div>
 
--- paginas/principal/register.php
+++ paginas/principal/register.php
@@ -1,3 +1,5 @@
+<script src="/scripts/validacao.js"></script>
+
 <div class="row justify-content-md-center">
 	<div class="col-lg-8">
 		<h4 class="mb-3">Dados pessoais</h4>
@@ -22,13 +24,13 @@
 			<div class="mb-3">
 				<label for="nickname">Empresa</label>
 				<div class="input-group">
-				<div class="input-group-prepend">
-					<span class="input-group-text">&reg;</span>
-				</div>
-				<input type="text" class="form-control" id="nickname" placeholder="Empresa" required>
-				<div class="invalid-feedback" style="width: 100%;">
-					Sua Empresa é obrigatório.
-				</div>
+					<div class="input-group-prepend">
+						<span class="input-group-text">&reg;</span>
+					</div>
+					<input type="text" class="form-control" id="nickname" placeholder="Empresa" required>
+					<div class="invalid-feedback" style="width: 100%;">
+						Sua Empresa é obrigatório.
+					</div>
 				</div>
 			</div>
 
@@ -36,7 +38,7 @@
 				<label for="email">Email</label>
 				<input type="email" class="form-control" id="email" placeholder="fulano@exemplo.com" required>
 				<div class="invalid-feedback">
-				Por favor, insira um endereço de e-mail válido.
+					Por favor, insira um endereço de e-mail válido.
 				</div>
 			</div>
 
@@ -44,7 +46,7 @@
 				<label for="senha">Senha</label>
 				<input type="password" class="form-control" id="senha" placeholder="" required>
 				<div class="invalid-feedback">
-				Por favor, insira uma senha válida.
+					Por favor, insira uma senha válida.
 				</div>
 			</div>
 
@@ -52,7 +54,7 @@
 				<label for="endereco">Endereço</label>
 				<input type="text" class="form-control" id="endereco" placeholder="Rua dos bobos, nº 0" required>
 				<div class="invalid-feedback">
-				Por favor, insira seu endereço.
+					Por favor, insira seu endereço.
 				</div>
 			</div>
 
@@ -60,96 +62,68 @@
 				<label for="endereco2">CPF/CNPJ</label>
 				<input type="text" class="form-control" id="cpfcnpj" placeholder="000.000.000-00" required>
 				<div class="invalid-feedback">
-				Seu CPF/CNPJ é obrigatório.
+					Seu CPF/CNPJ é obrigatório.
 				</div>
 			</div>
 
 			<div class="row">
 				<div class="col-md-5 mb-3">
-				<label for="pais">País</label>
-				<select class="custom-select d-block w-100" id="pais" required>
-					<option value="">Escolha...</option>
-					<option>Brasil</option>
-				</select>
-				<div class="invalid-feedback">
-					Por favor, escolha um país válido.
-				</div>
+					<label for="pais">País</label>
+					<select class="custom-select d-block w-100" id="pais" required>
+						<option value="">Escolha...</option>
+						<option>Brasil</option>
+					</select>
+					<div class="invalid-feedback">
+						Por favor, escolha um país válido.
+					</div>
 				</div>
 				<div class="col-md-4 mb-3">
-				<label for="estado">Estado</label>
-				<select class="custom-select d-block w-100" id="estado" required>
-					<option value="">Escolha...</option>
-					<option>Acre</option>
-				</select>
-				<div class="invalid-feedback">
-					Por favor, insira um estado válido.
-				</div>
+					<label for="estado">Estado</label>
+					<select class="custom-select d-block w-100" id="estado" required>
+						<option value="">Escolha...</option>
+						<option>Acre</option>
+					</select>
+					<div class="invalid-feedback">
+						Por favor, insira um estado válido.
+					</div>
 				</div>
 				<div class="col-md-3 mb-3">
-				<label for="cep">CEP</label>
-				<input type="text" class="form-control" id="cep" placeholder="" required>
-				<div class="invalid-feedback">
-					É obrigatório inserir um CEP.
+					<label for="cep">CEP</label>
+					<input type="text" class="form-control" id="cep" placeholder="" required>
+					<div class="invalid-feedback">
+						É obrigatório inserir um CEP.
+					</div>
 				</div>
+			</div>
+			<hr class="mb-4">
+
+			<h4 class="mb-3">Pagamento</h4>
+
+			<div class="d-block my-3">
+				<div class="custom-control custom-radio">
+					<input id="gratis" name="paymentMethod" type="radio" class="custom-control-input" checked required>
+					<label class="custom-control-label" for="gratis">Grátis</label>
 				</div>
-	</div>
-	<hr class="mb-4">
+				<div class="custom-control custom-radio">
+					<input id="credito" name="paymentMethod" type="radio" class="custom-control-input" required>
+					<label class="custom-control-label" for="credito">Cartão de crédito</label>
+				</div>
+				<div class="custom-control custom-radio">
+					<input id="debito" name="paymentMethod" type="radio" class="custom-control-input" required>
+					<label class="custom-control-label" for="debito">Cartão de débito</label>
+				</div>
+				<div class="custom-control custom-radio">
+					<input id="paypal" name="paymentMethod" type="radio" class="custom-control-input" required>
+					<label class="custom-control-label" for="paypal">PayPal</label>
+				</div>
+				<div class="custom-control custom-radio">
+					<input id="boleto" name="paymentMethod" type="radio" class="custom-control-input" required>
+					<label class="custom-control-label" for="boleto">Boleto</label>
+				</div>
+			</div>
 
-	<h4 class="mb-3">Pagamento</h4>
-
-	<div class="d-block my-3">
-		<div class="custom-control custom-radio">
-		<input id="gratis" name="paymentMethod" type="radio" class="custom-control-input" checked required>
-		<label class="custom-control-label" for="gratis">Grátis</label>
-		</div>
-		<div class="custom-control custom-radio">
-		<input id="credito" name="paymentMethod" type="radio" class="custom-control-input" required>
-		<label class="custom-control-label" for="credito">Cartão de crédito</label>
-		</div>
-		<div class="custom-control custom-radio">
-		<input id="debito" name="paymentMethod" type="radio" class="custom-control-input" required>
-		<label class="custom-control-label" for="debito">Cartão de débito</label>
-		</div>
-		<div class="custom-control custom-radio">
-		<input id="paypal" name="paymentMethod" type="radio" class="custom-control-input" required>
-		<label class="custom-control-label" for="paypal">PayPal</label>
-		</div>
-		<div class="custom-control custom-radio">
-		<input id="boleto" name="paymentMethod" type="radio" class="custom-control-input" required>
-		<label class="custom-control-label" for="boleto">Boleto</label>
-		</div>
-	</div>
-	
-	<hr class="mb-4">
-	<button class="btn btn-outline-primary btn-lg btn-block" type="submit">Enviar</button>
-	</form>
+			<hr class="mb-4">
+			<button class="btn btn-outline-primary btn-lg btn-block" type="submit">Enviar</button>
+		</form>
 	</div>
 </div>
-
-<script>
-	// Exemplo de JavaScript para desativar o envio do formulário, se tiver algum campo inválido.
-	(function() {
-		'use strict';
-
-		window.addEventListener('load', function() 
-		{
-			// Selecione todos os campos que nós queremos aplicar estilos Bootstrap de validação customizados.
-			var forms = document.getElementsByClassName('needs-validation');
-
-			// Faz um loop neles e previne envio
-			var validation = Array.prototype.filter.call(forms, function(form) 
-			{
-				form.addEventListener('submit', function(event) 
-				{
-					if (form.checkValidity() === false) 
-					{
-						event.preventDefault();
-						event.stopPropagation();
-					}
-
-					form.classList.add('was-validated');
-				}, false);
-			});
-		}, false);
-	})();
-</script>
