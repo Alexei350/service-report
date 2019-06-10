352735e433f48968bff7803e96999b9e1fe9ad87
Alexei <alexeisecretti@hotmail.com>
2019-06-10 15:55:40 -0300

    Descatadas mudanças na tela de login

--- paginas/login.php
+++ paginas/login.php
@@ -2,37 +2,29 @@
 
 <body class="text-center">
 	<main role="main" class="inner cover">
-		<!-- <div class="row justify-content-md-center">
-			<div class="col col-sm-4">
-				<div class="card">
-					<article class="card-body"> -->
-						<form class="form-signin" action="database/sessao_login.php" method="post">
-							<!--Cabeçalho do login-->
-							<img class="mb-4" id="img_login" src="img/login.png" alt="" width="128" height="128">
-							<h1 class="h3 mb-3 font-weight-normal">Login</h1>
+		<form class="form-signin" action="database/sessao_login.php" method="post">
+			<!--Cabeçalho do login-->
+			<img class="mb-4" id="img_login" src="img/login.png" alt="" width="128" height="128">
+			<h1 class="h3 mb-3 font-weight-normal">Login</h1>
 
-							<!--Email-->
-							<label for="inputEmail" class="sr-only">Email</label>
-							<input type="email" id="inputEmail" name="email" class="form-control" placeholder="Ex: usuario@dominio.com" required autofocus>
+			<!--Email-->
+			<label for="inputEmail" class="sr-only">Email</label>
+			<input type="email" id="inputEmail" name="email" class="form-control" placeholder="Ex: usuario@dominio.com" required autofocus>
 
-							<!--Senha-->
-							<label for="inputPassword" class="sr-only">Senha</label>
-							<input type="password" id="inputPassword" name="senha" class="form-control" placeholder="Digite sua senha" required>
+			<!--Senha-->
+			<label for="inputPassword" class="sr-only">Senha</label>
+			<input type="password" id="inputPassword" name="senha" class="form-control" placeholder="Digite sua senha" required>
 
-							<!--Lembrar usuário-->
-							<div class="checkbox mb-3">
-								<label>
-									<input type="checkbox" value="remember-me"> Lembrar usuário
-								</label>
-							</div>
-
-							<!--Botão de aceitar-->
-							<button class="btn btn-lg btn-outline-primary btn-block" type="submit">Conectar</button>
-							<a role="button" href="/register" class="btn btn-md text-primary">Não possui uma conta?</button>
-						</form>
-					<!-- </article>
-				</div>
+			<!--Lembrar usuário-->
+			<div class="checkbox mb-3">
+				<label>
+					<input type="checkbox" value="remember-me"> Lembrar usuário
+				</label>
 			</div>
-		</div> -->
+
+			<!--Botão de aceitar-->
+			<button class="btn btn-lg btn-outline-primary btn-block" type="submit">Conectar</button>
+			<a role="button" href="/register" class="btn btn-md text-primary">Não possui uma conta?</button>
+		</form>
 	</main>
 </body>
