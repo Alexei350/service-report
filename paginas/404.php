f5a04b5479d520f75335a250e5389cf417463c95
Alexei <alexeisecretti@hotmail.com>
2019-06-14 11:17:11 -0300

    Finaliza página de erro 404

---
+++ paginas/404.php
@@ -0,0 +1,22 @@
+<div class='row justify-content-md-center'>
+    <div class='col-lg-5'>
+        <!-- Imagem do topo da página -->
+        <div id='top_element'>
+            <img class='d-block mx-auto mb-4' id='img_404' src='img/404.png' alt='' width='100' height='100'>
+        </div>
+            
+        <!-- Título -->
+        <h1 class='cover-heading'>Página não encontrada</h1>
+
+        <p class='lead'>
+            <br>
+            A página que você está buscando não existe,<br>
+            verifique se o nome digitado está correto<br>
+            ou volte à página inicial
+        </p>
+
+        <p class='lead'>
+            <a role='button' href='/' class='btn btn-lg text-primary shadow-none'><i class='fas fa-angle-double-left'></i> Voltar à página inicial</a>
+        </p>
+    </div>
+</div>
